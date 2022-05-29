46e57a99b0f7d1c34caf65e8cb8f5fe078ff71d8
jams <36876904+jamsge@users.noreply.github.com>
2022-05-29 02:13:05 -0700

    better handling of concurrent submission requests

--- main.py
+++ main.py
@@ -30,10 +30,12 @@ async def hello(interaction: discord.Interaction):
 @app_commands.describe(attachment="The code to submit")
 async def submit(interaction: discord.Interaction, attachment: discord.Attachment):
     print("recieved submission")
-    await interaction.response.send_message(f'Thanks for uploading {attachment.filename}!', ephemeral=True)
-    await interaction.followup.send(f'The file uploaded was: {attachment.content_type}', ephemeral=True)
+    await interaction.response.send_message(f'Thanks for uploading {attachment.filename}!')
+    await interaction.followup.send(f'The file uploaded was: {attachment.content_type}')
     submission = await submitAttachmentToLeetcode(attachment)
-    # check if submission["err"] is true
-    await interaction.followup.send(content="_ _", embed=createSubmissionEmbed(details=submission["details"], uploader_name=interaction.user.name))
+    if submission["err"]:
+        await interaction.followup.send(embed=createSubmissionEmbed(msg=submission["msg"], uploader_name=interaction.user.name))
+    elif not submission["err"]:
+        await interaction.followup.send(embed=createSubmissionEmbed(details=submission["details"], uploader_name=interaction.user.name))
 
 client.run(BOT_TOKEN)
