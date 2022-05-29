637551f239ec1792f7c8a2610036a98b1416712d
Benjamin Ung <Benjaminung@cpp.edu>
2022-05-29 12:52:13 -0700

    added struct for user and class for manipulate dict of users

--- personal_stats/README.md
+++ personal_stats/README.md
@@ -1,8 +1,14 @@
 # BroncoderBot
 
 stats to track per person:
+current month's points
+    - use point system
+total number of points
+    -
 how many problems solved (aka. days active)
 -what kind of problems (how many easys, medium, and hards)
 number of times won (monthly competition)
 number of times first(first to submit)
-total number of points
+
+create a class object to store all these and have a dictionary to store these.
+- can be created when users enroll
--- personal_stats/stats.py
+++ personal_stats/stats.py
@@ -1,2 +1,61 @@
-print("Hello World")
-#stats to track
+
+#stats to be tracked
+
+class user:
+    def __init__(self) -> None:
+        self.totalPoints = 0
+        self.problemsSolved = 0 #also days committed
+        self.easy = 0
+        self.medium = 0
+        self.hard = 0
+        self.won = 0
+        self.first = 0
+
+    def toString(self):
+        pass
+
+#make a dictionary using user name as a key
+#write to file? to store if bot crashes
+class stats:
+    population: dict
+    population = {}
+
+    #ripped from points.py
+    def __init__(self):
+        if stats.__instance == None:
+            stats.__instance = self
+        else:
+            raise Exception("[ERROR]: Cannot create another instance of class: Points. Singlton implemented.")
+
+    @staticmethod
+    def get_instance():
+        if stats.__instance == None:
+            stats.__instance = stats()    
+        return stats.__instance
+
+    def addUser(self, userID: int): #add user upon getting role / leave stats if role removed?
+        userID = str(userID)
+        if self.population.get(userID) == None:
+            self.population[userID] = user()
+
+    def getUser(self, userID: int):
+        userID = str(userID)
+        return self.population[userID]
+
+    def updatePoints(self, userID: int, difficulty: int, first: bool):
+        baseValue, multiplier, firstBonus= 1,2,0
+        if first:
+            firstBonus = 1
+        userID = str(userID)
+        user = self.population[userID]
+        user.problemsSolved += 1
+        
+        user.totalPoints += baseValue + difficulty*multiplier + firstBonus
+
+        if difficulty == "1":
+            user.easy += 1
+        elif difficulty == "2":
+            user.medium += 1
+        elif difficulty == "3":
+            user.hard += 1
+        
