35487355b7966b84de529395547440dd46823338
Pallav <43105309+notpallav@users.noreply.github.com>
2022-05-30 19:43:15 -0700

    typo

--- command_handling/admin/__init__.py
+++ command_handling/admin/__init__.py
@@ -7,18 +7,25 @@ from persistent_store import PersistentStore
 
 store = PersistentStore.get_instance()
 
+
 async def __admin_only(interaction: discord.Interaction):
     guild_id = interaction.guild_id
 
     # Defaults to administrator permissions if no admin role assigned
-    if not guild_id in store or 'admin_role' not in store[guild_id]:
+    if not guild_id in store or "admin_role" not in store[guild_id]:
         if interaction.user.resolved_permissions.administrator:
             return True
-    elif interaction.guild.get_role(store[guild_id][admin_role]) in interaction.user.roles:
+    elif (
+        interaction.guild.get_role(store[guild_id]["admin_role"])
+        in interaction.user.roles
+    ):
         return True
 
-    await interaction.response.send_message('Admin role required to invoke this command', ephemeral=True)
+    await interaction.response.send_message(
+        "Admin role required to invoke this command", ephemeral=True
+    )
     return False
+
 
 def map_commands(tree):
     for cmd in [getattr(commands, name) for name in commands.__all__]:
