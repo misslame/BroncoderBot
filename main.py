484f570a01fefd286c6a912bc9fa24fe4017876a
Benjamin Ung <Benjaminung@cpp.edu>
2022-06-09 14:25:35 -0700

    implemented random hellos using array. web scrap not practical

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from multiprocessing.dummy import Array
 import sys
 import traceback
 import discord
@@ -20,6 +21,8 @@ from command_handling.first_handler import get_first_stats
 from command_handling.timeout_handler import readable
 from command_handling import admin as admin_commands
 
+    # -------- Command Handling -----------
+import random
     # -------- Problem Submission ---------
 from messages.problem_view import ProblemView
 from messages.embeds import createSubmissionEmbed, createProblemEmbed, getProblemEmbeds
@@ -104,11 +107,18 @@ async def on_ready():
 
 ****************************************************'''
 
+greeting = ["greeting", "aloha", "good afternoon", "good day", "good evening", "good morning", "good night", "greetings",
+    "guten Tag", "hello", "hey", "hola", "how's it going?", "how's it hanging?", "konnichi wa", "saulutions", "sup",
+    "what's happening?", "what's new?", "what's up?", "whazzup!!!", "yo holla how do?", "how goes it?", "how ya doing?",
+     "sup", "sup, b?", "what's cooking?", "what's crack-a-lackin", "what's cracking?", "What's in the bag?", "what's popping",
+     "what's shaking?", "what's the dizzle?", "what's the haps?", "what's the rumpus?", "what's up?", "yello" ]
+
 ''' ---------- FUN ---------- '''
 @tree.command(description="Say hello.")
-@app_commands.checks.cooldown(1, COOLDOWN_SECONDS)
+#@app_commands.checks.cooldown(1, COOLDOWN_SECONDS)
 async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Hi, {interaction.user.mention}")
+    entry = random.randrange(0,len(greeting) - 1)
+    await interaction.response.send_message(f'{greeting[entry]} {interaction.user.mention}')
 
 ''' ---------- PROBLEM SUBMISSION ---------- '''
 
