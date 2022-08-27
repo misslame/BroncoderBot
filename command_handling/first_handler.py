257d664890038fc66c7503cb9f588bdc407d2a2f
Lint Action <lint-action@samuelmeuli.com>
2022-08-27 21:43:45 +0000

    Fix code style issues with Black

--- command_handling/first_handler.py
+++ command_handling/first_handler.py
@@ -6,7 +6,6 @@ from participant_data_handling.participant_data import ParticipantData
 from command_handling.announcement_handler import END_COMPETITION_ANNOUNCEMENT_TIME
 
 
-
 def get_first_stats(interaction: discord.Interaction):
     days_left = monthrange(date.today().year, date.today().month)[1] - date.today().day
     response_message = ""
@@ -21,16 +20,17 @@ def get_first_stats(interaction: discord.Interaction):
     ):
         first_ids = ParticipantData.get_instance().get_firsts()
 
-        #check case: there are users logged but no one has gotten points yet
-        if(ParticipantData.get_instance().get_points(first_ids[0]) > 0):
+        # check case: there are users logged but no one has gotten points yet
+        if ParticipantData.get_instance().get_points(first_ids[0]) > 0:
 
             # get list of users in first place
             first_places = list(
                 map(
-                    lambda id: interaction.guild.get_member(int(id)).display_name, first_ids
+                    lambda id: interaction.guild.get_member(int(id)).display_name,
+                    first_ids,
                 )
             )
-            
+
             # check if user is in first place
             user_is_first = first_places.__contains__(interaction.user.display_name)
             # remove user from list
@@ -50,27 +50,29 @@ def get_first_stats(interaction: discord.Interaction):
                     if i < len(first_places) - 2:
                         first_place_message += ", "
 
-    #comparing user score with list of 1st places
-            if user_is_first:  #case 1: only you are 1st place
+            # comparing user score with list of 1st places
+            if user_is_first:  # case 1: only you are 1st place
                 response_message = f"You are first place! Keep it up, you have {ParticipantData.get_instance().get_points(interaction.user.id)} point(s)!\n"
-                
-                #case 2: tied with other users
+
+                # case 2: tied with other users
                 if not len(first_places) == 0:
                     response_message = f"You are tied with {first_place_message}\n"
 
-            else: #case 3: user is not in first place
+            else:  # case 3: user is not in first place
                 points_behind = ParticipantData.get_instance().get_points(
                     first_ids[0]
                 ) - ParticipantData.get_instance().get_points(interaction.user.id)
                 verb = "is" if len(first_places) == 1 else "are"
                 response_message = f"{first_place_message} {verb} in first place! You are {points_behind} point(s) behind them!\n"
         else:
-            response_message = f'Everyone has 0 points. You have a chance to be the winner!\n'
-    #if there are no participants logged,
+            response_message = (
+                f"Everyone has 0 points. You have a chance to be the winner!\n"
+            )
+    # if there are no participants logged,
     else:
-        response_message = f'There are no participants as of now.\n'
+        response_message = f"There are no participants as of now.\n"
 
-#printing out days left output
+    # printing out days left output
     if days_left > 0:
         response_message += (
             f"There are {days_left} days left till the competition ends!\n"
